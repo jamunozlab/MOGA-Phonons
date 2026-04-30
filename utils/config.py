84dcde2e99ab9a2c89b37b8ca9c1361e64f8c401
Jorge Munoz <jamunoz@utep.edu>
2026-04-30 13:33:53 -0600

    Update executable name

--- utils/config.py
+++ utils/config.py
@@ -5,6 +5,6 @@ global paths to required directories and executable file name
 home_path = '/home/jamunoz/MOGA-Phonons'
 simulations_path = '/home/jamunoz/MOGA-Phonons/simulations/'
 executables_path = '/home/jamunoz/MOGA-Phonons/executables/'
-executable = 'pygad_module_2_6_50_mod_parallel.py'
+executable = 'pygad_module_parallel.py'
 experiments_path = '/home/jamunoz/MOGA-Phonons/experiments/'
 other_files = []
