56b382c6037d299b1c74ce480757c203cf6d0f9c
Jorge Munoz <jamunoz@utep.edu>
2026-04-29 23:14:18 -0600

    close to last utep

---
+++ new_experiment.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+
+"""
+This script is responsible for iterating over the variables that will be modified in an experiment,
+creating a directory for each combination of interest with all the files necessary to run an individual
+simulation in that directory, and submitting the job to the Slurm worload manager.
+
+The user must provide code to modify the variables of interest in sim_params_dict and sbatch_options_dict,
+as well as a description of the experiment. The function new_simulation_from_default should be called
+once for each simulation. The script should be executed from the command line. 
+"""
+
+import argparse
+
+from utils.new_simulation_from_default import new_simulation_from_default
+from utils.dir_structure_utils import find_next_available_file
+from utils.config import simulations_path, executables_path, executable, experiments_path, other_files
+import utils.default_dictionaries as default_dictionaries
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+                    prog='new_experiment',
+                    description='Create new experiment. User must provide the name of a dictionary' +
+                                    'in default_dictionaries.py with input parameters and default values.',
+                    epilog='')
+    
+    parser.add_argument('default_input_dictionary_name') # positional argument
+    parser.add_argument('-t', '--test', action='store_false')  # on/off flag
+    
+    submit_job=False
+
+    args = parser.parse_args()
+    submit_job = args.test
+    default_input_dictionary = getattr(
+        default_dictionaries,
+        args.default_input_dictionary_name
+    )
+    
+    simulation_ids = []
+
+
+#### SCRIPT SHOULD BE BELOW ####      
+    
+    experiment_description = (
+        "MOGA Jakar sweep\n"
+        "atomic_masses: 50–100 step 10\n"
+        "a_val: 2.0–3.2 step 0.2\n"
+    )
+    
+    count = 0
+
+    # Mass: 50 → 100 (inclusive), step 10
+    masses = list(range(50, 101, 10))
+
+    # a_val: 2.0 → 3.2 (inclusive), step 0.2
+    a_vals = [round(2.0 + 0.2*i, 2) for i in range(int((3.2 - 2.0)/0.2) + 1)]
+
+    for mass in masses:
+        for a_val in a_vals:
+
+            sim_params_dict = {
+                "atomic_masses": [float(mass)],
+                "a_val": float(a_val)
+            }
+
+            sbatch_options_dict = {}  # use defaults
+
+            simulation_id = new_simulation_from_default(
+                default_input_dictionary,
+                simulations_path,
+                executables_path,
+                executable,
+                sim_params_dict,
+                sbatch_options_dict,
+                other_files=other_files,
+                submit_job=submit_job
+            )
+
+            simulation_ids.append(simulation_id)
+            count += 1
+        
+    experiment_id = find_next_available_file('experiment', experiments_path)
+
+#### SCRIPT SHOULD BE ABOVE ####  
+
+    lines = experiment_description + str('\n')
+    for simulation_id in simulation_ids:
+        lines = lines + simulation_id + str('\n')
+
+    with open(experiments_path + experiment_id, "w") as f:
+        f.writelines(lines)
+    
+    print(experiment_id)
+    print(simulation_ids)
+    
--- utils/write_input_from_default.py
+++ utils/write_input_from_default.py
@@ -32,9 +32,31 @@ def generate_input_lines(param_dict):
         lines.append(first_part + second_part + str('\n'))
     return lines
 
+def write_bcc_poscar(simulation_path, a_val, system_name="Fe"):
+    lines = [
+        f"For VASP. System= {system_name}\n",
+        f"{a_val:.16f}\n",
+        "1.0000000000000000 0.0000000000000000 0.0000000000000000\n",
+        "0.0000000000000000 1.0000000000000000 0.0000000000000000\n",
+        "0.0000000000000000 0.0000000000000000 1.0000000000000000\n",
+        "2\n",
+        "Direct\n",
+        "0.0000000000000000 0.0000000000000000 0.0000000000000000\n",
+        "0.5000000000000000 0.5000000000000000 0.5000000000000000\n",
+    ]
+
+    with open(simulation_path + "POSCAR", "w") as f:
+        f.writelines(lines)
+
 def write_input_from_default(default, simulation_path, **kwargs):
     simulation_parameters_dictionary = generate_input_from_default(default, **kwargs)
     input_lines = generate_input_lines(param_dict=simulation_parameters_dictionary)
-    f = open(simulation_path+"inputc", "w")
-    f.writelines(input_lines)
-    f.close()
+
+    with open(simulation_path + "inputc", "w") as f:
+        f.writelines(input_lines)
+
+    if "a_val" in simulation_parameters_dictionary:
+        write_bcc_poscar(
+            simulation_path,
+            simulation_parameters_dictionary["a_val"]
+        )
